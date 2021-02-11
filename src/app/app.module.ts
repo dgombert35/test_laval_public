931e56921539d625332dfc6cb96b4d6b7d43b058
dgombert35 <damiengombert@hotmail.fr>
2021-02-11 22:25:30 +0100

    add modules + service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomePageModule } from './full-Page/home-Page/home-page.module';
 
+import { TokenService } from './service/core/token.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    HomePageModule
+    HomePageModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BrowserModule,
   ],
-  providers: [],
+  providers: [TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
